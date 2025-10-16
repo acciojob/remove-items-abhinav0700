288e17b27d24dc201b471b7c0ea8e3ce1bcb50d8
Abhinav M <130998983+abhinav0700@users.noreply.github.com>
2025-10-16 16:22:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let colorselect = document.getElementById("colorSelect");
+let button = document.querySelector('input[type="button"]');
+
+button.addEventListener("click", function() {
+  let index = colorselect.selectedIndex;
+  colorselect.remove(index);
+});
